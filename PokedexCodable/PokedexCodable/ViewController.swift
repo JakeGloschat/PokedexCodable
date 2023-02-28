bd07a0cd391dc5e07af1d1fe6e906ef4775bff0b
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-28 09:26:10 -0700

    Adds initial project.

---
+++ PokedexCodable/PokedexCodable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexCodable
+//
+//  Created by Jake Gloschat on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
