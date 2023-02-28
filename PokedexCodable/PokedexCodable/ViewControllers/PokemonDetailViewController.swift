70a4f5cdc176195f5140edc6058bf5a0631b2f91
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-28 15:45:12 -0700

    Updated the cellForRowAt to display the array of moves for each pokemon.

--- PokedexCodable/PokedexCodable/ViewControllers/PokemonDetailViewController.swift
+++ PokedexCodable/PokedexCodable/ViewControllers/PokemonDetailViewController.swift
@@ -38,7 +38,7 @@ class PokemonDetailViewController: UIViewController {
             switch result {
             case .success(let image):
                 DispatchQueue.main.async {
-                    self.pokemonNameLabel.text = pokemon.name
+                    self.pokemonNameLabel.text = pokemon.name.capitalized
                     self.pokemonIdLabel.text = "\(pokemon.id)"
                     self.pokemonSpriteImageView.image = image
                     self.pokemonMovesTableView.reloadData()
@@ -53,11 +53,15 @@ class PokemonDetailViewController: UIViewController {
 
 extension PokemonDetailViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return pokemon?.moves.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
+        let move = pokemon?.moves[indexPath.row].move
+        var config = cell.defaultContentConfiguration()
+        config.text = move?.name.capitalized
+        cell.contentConfiguration = config
         
         return cell
     }
